e74a2eaf43d425d7a8f1bb0799ee4f1117366517
seokhyeon12345@gmail.com <seokhyeon12345@gmail.com>
2022-06-26 23:40:00 +0900

    FEAT: add animation to order items page when click order item add fade & slide animation

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -14,58 +14,93 @@ class OrderItem extends StatefulWidget {
   State<OrderItem> createState() => _OrderItemState();
 }
 
-class _OrderItemState extends State<OrderItem> {
+class _OrderItemState extends State<OrderItem>
+    with SingleTickerProviderStateMixin {
   var _expanded = false;
+  AnimationController _animationController;
+  Animation<double> _opacityAnimation;
+
+  @override
+  void initState() {
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 300),
+    );
+    _opacityAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(
+        parent: _animationController,
+        curve: Curves.easeIn,
+      ),
+    );
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.all(10),
-      child: Column(
-        children: <Widget>[
-          ListTile(
-            title: Text('\$ ${widget.order.amount}'),
-            subtitle: Text(
-              DateFormat('dd/MM/yyyy hh:mm').format(widget.order.dateTime),
-            ),
-            trailing: IconButton(
-              icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
-              onPressed: () {
-                setState(() {
-                  _expanded = !_expanded;
-                });
-              },
-            ),
-          ),
-          if (_expanded)
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
-              height: min(widget.order.products.length * 20.0 + 10, 100),
-              child: ListView(
-                children: widget.order.products
-                    .map((prod) => Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: <Widget>[
-                            Text(
-                              prod.title,
-                              style: TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            Text(
-                              '${prod.quantity}x \$${prod.price}',
-                              style: TextStyle(
-                                fontSize: 18,
-                                color: Colors.grey,
-                              ),
-                            )
-                          ],
-                        ))
-                    .toList(),
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 300),
+      curve: Curves.easeIn,
+      height:
+          _expanded ? min(widget.order.products.length * 20.0 + 110, 195) : 95,
+      constraints: BoxConstraints(
+          minHeight: _expanded
+              ? min(widget.order.products.length * 20.0 + 105, 195)
+              : 95),
+      child: Card(
+        margin: EdgeInsets.all(10),
+        child: Column(
+          children: <Widget>[
+            ListTile(
+              title: Text('\$ ${widget.order.amount}'),
+              subtitle: Text(
+                DateFormat('dd/MM/yyyy hh:mm').format(widget.order.dateTime),
               ),
-            )
-        ],
+              trailing: IconButton(
+                icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
+                onPressed: () {
+                  setState(() {
+                    if (_expanded)
+                      _animationController.reverse();
+                    else
+                      _animationController.forward();
+                    _expanded = !_expanded;
+                  });
+                },
+              ),
+            ),
+            if (_expanded)
+              FadeTransition(
+                opacity: _opacityAnimation,
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+                  height: min(widget.order.products.length * 20.0 + 10, 100),
+                  child: ListView(
+                    children: widget.order.products
+                        .map((prod) => Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                Text(
+                                  prod.title,
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Text(
+                                  '${prod.quantity}x \$${prod.price}',
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    color: Colors.grey,
+                                  ),
+                                )
+                              ],
+                            ))
+                        .toList(),
+                  ),
+                ),
+              )
+          ],
+        ),
       ),
     );
   }
